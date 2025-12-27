e756746f82dda7416e0d38a3465bd6706f3345b8
nurulabsr <absar.ru@yandex.com>
2025-12-28 01:36:38 +0600

    updated

---
+++ HelloWorld/datatype.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+//	variable name must be include latter, number, underscore ( _ )
+// variable will not strat number like 1num, 
+// allowed _num, num_1, or numOne
+// reserve words are'nt allowed
+	int num_1;
+	float 
+}
