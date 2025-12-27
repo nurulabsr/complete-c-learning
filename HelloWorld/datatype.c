0fb2097f60e40c555bf14bc26f4d9741c2e2b632
nurulabsr <absar.ru@yandex.com>
2025-12-28 02:26:09 +0600

    updated memory location

--- HelloWorld/datatype.c
+++ HelloWorld/datatype.c
@@ -15,6 +15,24 @@ void main(){
 	long double num_9 = 3.141592653589793; 
 
 int *address_of_num_1 = &num_1;  
+float *address_of_num_2 = &num_2;
+short *address_of_num_3 = &num_3;
+long *address_of_num_4 = &num_4;
+unsigned *address_of_num_5 = &num_5;
+signed *address_of_num_6 = &num_6;
+double *address_of_num_7 = &num_7;
+char *address_of_num_8 = &num_8;
+long double *address_of_num_9 = &num_9;
+
 printf("address of first number: %p\n", (void*)address_of_num_1);  
+printf("address of second number: %p\n", (void*)address_of_num_2); 
+printf("address of third number: %p\n", (void*)address_of_num_3); 
+printf("address of forth number: %p\n", (void*)address_of_num_4); 
+printf("address of fifth number: %p\n", (void*)address_of_num_5); 
+printf("address of sixth number: %p\n", (void*)address_of_num_6); 
+printf("address of seven number: %p\n", (void*)address_of_num_7); 
+printf("address of eight number: %p\n", (void*)address_of_num_8); 
+printf("address of nine number: %p\n", (void*)address_of_num_9); 
+
 	
 }
