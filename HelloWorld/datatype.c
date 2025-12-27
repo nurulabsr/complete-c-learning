4b317d59e398ac3889674cac6d9cb76ae50ce754
nurulabsr <absar.ru@yandex.com>
2025-12-28 02:16:20 +0600

    updated

--- HelloWorld/datatype.c
+++ HelloWorld/datatype.c
@@ -4,6 +4,17 @@ void main(){
 // variable will not strat number like 1num, 
 // allowed _num, num_1, or numOne
 // reserve words are'nt allowed
-	int num_1;
-	float 
+	int num_1 =99;
+	float num_2 = 100.5;
+	short num_3 = 100;
+	long num_4 = 1000000L;
+	unsigned int num_5 = 50;
+	signed int num_6 = -50;
+	double num_7 = 50000.50;
+	char num_8 = '8';
+	long double num_9 = 3.141592653589793; 
+
+int *address_of_num_1 = &num_1;  
+printf("address of first number: %p\n", (void*)address_of_num_1);  
+	
 }
